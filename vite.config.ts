f46c611b225453ebfc5f0b7f8bb7d2e3094fd7f0
Mattias Duarte <duartemattias4@gmail.com>
2026-06-24 22:31:02 -0400

    bases de tanstack start, archivos estatucis y loaders

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import { defineConfig } from 'vite';
+import { devtools } from '@tanstack/devtools-vite';
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
-})
+    resolve: { tsconfigPaths: true },
+    plugins: [
+        devtools(),
+        tailwindcss(),
+        tanstackStart({
+            prerender: {
+                autoStaticPathsDiscovery: true,
+            },
+            pages: [
+                {
+                    path: '/properties',
+                    prerender: { enabled: true, outputPath: '/properties/index.html' },
+                }
+            ]
+        }),
+        viteReact(),
+    ],
+});
 
-export default config
+export default config;
